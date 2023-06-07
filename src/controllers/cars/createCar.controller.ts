1c128999af7f45250910d1a85758c4da23c7b43a
rafael <rafael.santos7520@gmail.com>
2023-06-07 17:36:31 -0300

    create uuid validate middleware

--- src/controllers/cars/createCar.controller.ts
+++ src/controllers/cars/createCar.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import createCarService from "../../services/cars/createCar.service";
-import { car, carRequest } from "../../interfaces/cars/car.interface";
+import { ICar, ICarRequest } from "../../interfaces/cars/car.interface";
 
 const createCarController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const data: carRequest = req.body;
-  const responce: car = await createCarService(data);
+  const data: ICarRequest = req.body;
+  const responce: ICar = await createCarService(data);
 
   return res.status(201).json(responce);
 };
--- src/services/cars/createCar.service.ts
+++ src/services/cars/createCar.service.ts
@@ -1,15 +1,15 @@
 import AppDataSource from "../../data-source";
 import { Car } from "../../entities";
-import { car, carRequest } from "../../interfaces/cars/car.interface";
+import { ICar, ICarRequest } from "../../interfaces/cars/car.interface";
 import { carSchema } from "../../schemas/car.schema";
 
-const createCarService = async (data: carRequest): Promise<car> => {
+const createCarService = async (data: ICarRequest): Promise<ICar> => {
   const carRepository = AppDataSource.getRepository(Car);
 
   const newCar: Car = carRepository.create(data);
   await carRepository.save(newCar);
 
-  const validatedCar: car = carSchema.parse(newCar);
+  const validatedCar: ICar = carSchema.parse(newCar);
 
   return validatedCar;
 };
