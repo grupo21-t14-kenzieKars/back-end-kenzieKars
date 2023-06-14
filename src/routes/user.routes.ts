9f96e91b9f4f10d79661cfb1af3ea025753b273e
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-06-14 13:44:45 -0300

    feat: added authentication middlewares to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,11 +5,12 @@ import { UserRequestSchema } from "../schemas/user.schema";
 import listUserController from "../controllers/users/listUser.controller";
 import updateUserController from "../controllers/users/updateUser.controller";
 import deleteUserController from "../controllers/users/deleteUser.controller";
+import ensureauthMiddleware from "../middlewares/ensureAuthMiddleware";
 
 const usersRoutes: Router = Router();
 
 usersRoutes.post("", verifySchemaMiddleware(UserRequestSchema), createUserController);
 usersRoutes.get("", listUserController);
-usersRoutes.patch("/id", updateUserController);
-usersRoutes.delete("/id", deleteUserController);
+usersRoutes.patch("/id", ensureauthMiddleware, updateUserController);
+usersRoutes.delete("/id", ensureauthMiddleware, deleteUserController);
 
